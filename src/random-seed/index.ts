3039a908489a62a67cab31bf8ec17687bec31a6c
= <=>
2019-12-23 13:53:47 -0800

    Clean up random-seed index

--- src/random-seed/index.ts
+++ src/random-seed/index.ts
@@ -1,7 +1,3 @@
-// TODO: Need to be able to instantiate multiple random_seed modules
-// TODO: Need to expose Typescript wrapper in front of addon, User should interact with ts wrapper
-//  > One issue is passing arrays from C to jS requires a buffer. Can abstract from user.
-//  > Another issue is handling endiness from uint8_t buffer -> int32_t. Can abstract from user.
 // TODO: Need to consolidate this module into a standalone module (push to npm_modules?)
 // TODO: Move tests for random seed to this module
 
@@ -9,6 +5,7 @@
 export class RandomGenerator {
     private _r:any;
     constructor() {
+        // NOTE: Required to reload module multiple times
         delete require.cache[require.resolve(`./build/Release/random_seed.node`)]
         this._r = require(`./build/Release/random_seed.node`);
     }
